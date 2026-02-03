46cc1ba888ff1f49f7a85ced16ec65d94788f1c4
Kaylee Dehncke <kade9897@colorado.edu>
2026-02-03 13:44:20 -0700

    initial setup

---
+++ website/resources/js/script.js
@@ -0,0 +1,27 @@
+// Fetch the navbar HTML and insert it into the page
+fetch('../layouts/navbar.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('navbar-placeholder').innerHTML = data;
+        let currentPage = window.location.pathname.split("/").pop().toLowerCase().split("?")[0].split("#")[0];
+        if (!currentPage) 
+        {
+            currentPage = "intro.html";
+        }
+        const navLinks = document.querySelectorAll('#navbar-placeholder .navbar-link');
+        navLinks.forEach(link => {
+            if (link.getAttribute('href').toLowerCase() === currentPage) 
+            {
+                link.classList.add('active');
+                link.setAttribute('aria-current', 'page');
+            } 
+            else 
+            {
+                link.classList.remove('active');
+                link.removeAttribute('aria-current');
+            }
+        });
+    })
+    .catch(error => console.error('Error loading navbar:', error));
+
+fetch('../../')
